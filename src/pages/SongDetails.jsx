e1ecb2bf3d04631ce25d62e627a6866d09c8d671
Luis <l.mino.bustos@gmail.com>
2022-10-14 13:48:21 -0300

    add Song Lirics Page

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -12,13 +12,16 @@ const SongDetails = () => {
 
   return (
     <div className="flex flex-col">
-      {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
+      <DetailsHeader artistId="" songData={songData} />
       <div className="mb-10">
         <h2 className="text-white text-3x1 font-bold">
           Lyrics
         </h2>
         <div className="mt-5">
-          {songData?.sections[1].type === 'LYRICS' ? songData.sections[1].text.map((line, i) => <p>{line}</p>) : <p>SOrry, no Lyrics found!</p>}
+          {songData?.sections[1].type === 'LYRICS'
+            ? songData.sections[1].text.map((line, i) => (
+              <p className="text-gray-400 text-base my-1">{line}</p>
+            )) : <p className="text-gray-400 text-base my-1">SOrry, no Lyrics found!</p>}
         </div>
       </div>
     </div>
